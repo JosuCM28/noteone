ba873ad2bf1cfb871c59e3dc8387f67478cb84dc
Josue Cruz Marin <cruzjosu57@gmail.com>
2026-03-29 14:42:54 -0600

    Prueba 2.3

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  // Necesario para que Next.js confíe en los headers del proxy inverso (Traefik)
-  // y genere URLs correctas (https) en lugar de http
-  experimental: {
-    trustHostHeader: true,
-  },
 };
 
 export default nextConfig;
